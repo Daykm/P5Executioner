ab0cd0082c9ee78a16d17049a6b4eadfc47e3bb8
Daykm <daykmsc@gmail.com>
2017-07-26 22:46:49 -0400

    Cleanup

--- app/src/main/kotlin/com/daykm/p5executioner/main/P5Module.kt
+++ app/src/main/kotlin/com/daykm/p5executioner/main/P5Module.kt
@@ -8,12 +8,10 @@ import com.daykm.p5executioner.skills.P5SkillsAdapter
 import com.daykm.p5executioner.view.Pageable
 import dagger.Module
 import dagger.Provides
-import javax.inject.Named
 
 @Module class P5Module {
     @Provides fun pool(): RecyclerView.RecycledViewPool = RecyclerView.RecycledViewPool()
     @Provides
-    @Named("pageables")
     fun adapters(fusion: P5FusionAdapter, personas: P5ListAdapter, skills: P5SkillsAdapter, info: InfoAdapter)
             : List<Pageable> = listOf(fusion, personas, skills, info)
 }
