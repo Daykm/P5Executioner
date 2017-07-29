67811dedcad0681c0c046f6e43d2831f66138778
daykm <daykmsc@gmail.com>
2017-07-28 22:45:12 -0400

    VARIANCE IS MAGIC

--- app/src/main/kotlin/com/daykm/p5executioner/view/PageableAdapter.kt
+++ app/src/main/kotlin/com/daykm/p5executioner/view/PageableAdapter.kt
@@ -7,7 +7,7 @@ import android.view.ViewGroup
 import javax.inject.Inject
 
 class PageableAdapter
-@Inject constructor(val pageables: List<Pageable>, val pool: RecyclerView.RecycledViewPool) :
+@Inject constructor(val pageables: List<@JvmSuppressWildcards Pageable>, val pool: RecyclerView.RecycledViewPool) :
         PagerAdapter() {
 
     override fun isViewFromObject(p0: View, p1: Any): Boolean = p0 === p1
