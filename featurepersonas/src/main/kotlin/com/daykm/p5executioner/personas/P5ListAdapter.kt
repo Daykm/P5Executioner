b0e1d8678acf1f42428a228f52bf9a63703fcbdf
Dayko <daykmdev@gmail.com>
2018-09-05 18:43:01 -0400

    Fix imports

--- featurepersonas/src/main/kotlin/com/daykm/p5executioner/personas/P5ListAdapter.kt
+++ featurepersonas/src/main/kotlin/com/daykm/p5executioner/personas/P5ListAdapter.kt
@@ -3,7 +3,9 @@ package com.daykm.p5executioner.personas
 import android.support.v7.app.AppCompatActivity
 import com.airbnb.epoxy.TypedEpoxyController
 import com.daykm.p5executioner.data.DataRepo
+import com.daykm.p5executioner.proto.Persona
 import io.reactivex.android.schedulers.AndroidSchedulers
+import io.reactivex.rxkotlin.subscribeBy
 import timber.log.Timber
 import javax.inject.Inject
 
